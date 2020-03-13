18dcbc01f2cb0b739e54035ad632ed0acc67db0e
Ozymandias314 <34351596+Ozymandias314@users.noreply.github.com>
2020-03-13 15:04:45 -0400

    another change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>Cthulhu!</h1>
-      <h2>Start waht editing to see some magic happen! MAGIC MAGIC now I change something here and I will cahnge it again</h2>
+      <h2>
+        Start waht editing to see some magic happen! MAGIC MAGIC now I change
+        something here and I will cahnge it again and again and again
+      </h2>
     </div>
   );
 }
