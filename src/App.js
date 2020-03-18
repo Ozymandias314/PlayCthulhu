5b0a6242c67994043c9a4514e7b24834e359526f
Ozymandias314 <34351596+Ozymandias314@users.noreply.github.com>
2020-03-18 10:52:04 -0400

    some weird stuff

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
+      <h1> </h1>
+      <h1>
       <h1>Cthulhu!</h1>
       <h2>
         Start waht editing to see some magic happen! MAGIC MAGIC now I change
